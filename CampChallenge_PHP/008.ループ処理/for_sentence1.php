abdd9ffbf81434b7d688bee9d13b2929a5df21c0
himeno yukihiro <bin.bin.pynchon@gmail.com>
2017-06-18 20:45:42 +0900

    ループ処理 for文-for文1を提出します。

---
+++ CampChallenge_PHP/008.ループ処理/for_sentence1.php
@@ -0,0 +1,24 @@
+  <?php
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+$total = 8;
+for ($i = 1; $i <= 20; $i++){
+  $total = $total * $i;
+  echo $total;
+  echo '<br>';
+}
+
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
