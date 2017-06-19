8a48b4022891aed41de72712945e81a3649d1f71
himeno yukihiro <bin.bin.pynchon@gmail.com>
2017-06-19 20:34:20 +0900

    引数-引数1を提出します。

---
+++ CampChallenge_PHP/009.関数/argument1.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Front to the WordPress application. This file doesn't do anything, but loads
+* wp-blog-header.php which does and tells WordPress to load the theme.
+*
+* @package WordPress
+*/
+function kansu($num) {
+  if($num % 2 == 0) {
+    echo $num. 'は偶数です。';}
+  else {
+    echo $num. 'は奇数です。';}
+}
+kansu(100);
+
+
+/**
+* Tells WordPress to load the WordPress theme and output it.
+*
+* @var bool
+*/
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
