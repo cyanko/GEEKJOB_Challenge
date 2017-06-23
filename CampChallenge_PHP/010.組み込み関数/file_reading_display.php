6e95499b1a910859be2fc73cdc2a2b3870732baa
himeno yukihiro <bin.bin.pynchon@gmail.com>
2017-06-23 19:35:12 +0900

    ファイル操作編-ファイルから読み出しと表示を提出します。

---
+++ CampChallenge_PHP/010.組み込み関数/file_reading_display.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Front to the WordPress application. This file doesn't do anything, but loads
+* wp-blog-header.php which does and tells WordPress to load the theme.
+*
+* @package WordPress
+*/
+ini_set( 'display_errors', 1 );
+$text = '姫野至廣です。28歳です。大阪府出身です。';
+
+// 一括書き込み
+file_put_contents('sample3.txt', $text);
+
+// 一括読み込み
+echo file_get_contents('sample3.txt');
+
+// * Tells WordPress to load the WordPress theme and output it.
+// *
+//define('WP_USE_THEMES', true);
+
+/*Loads the WordPress Environment and Template */
+//require( dirname( __FILE__ ) . '/wp-blog-header.php' );
